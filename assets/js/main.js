f821a98e9bd39398d8cf455b9c7a7078fe9bec72
mck <mck@naver.com>
2025-11-27 17:05:50 +0900

    feat: 메인 페이지 레이아웃을 전면 개편하고 주요 콘텐츠 섹션을 추가\ 주요 변경 내용: - 상단 하이라이트 배너 영역에 상품 리스트 섹션 추가. - 신규 상품 쇼케이스 섹션(배너 + 스와이퍼 조합) 추가. - 태블릿, 모바일 환경에 맞는 반응형 스타일 적용

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,16 +2,55 @@ document.addEventListener('DOMContentLoaded', function() {
   const body = document.querySelector('body');
   body.classList.add('main');
 
-  const mainVisualSlider = new Swiper('.main-visual > .swiper-container', {
+  const mainVisualSwiper = new Swiper('.main-visual > .swiper-container', {
     loop: true,      
     autoplay: {
-      delay: 5000, 
+      delay: 500000, 
       disableOnInteraction: false,
     },
     scrollbar: {
       el: '.main-visual .swiper-scrollbar',        
       draggable: true,
     },               
+  });
+
+  const bannerSectionSwiper = new Swiper('.banner-section > .swiper-container', {
+    grid: {
+      rows: 2,          
+      fill: 'row',      
+    },
+    slidesPerView: 2,
+    spaceBetween: 16,    
+    breakpoints: {
+      768: {
+        slidesPerView: 4,
+        spaceBetween: 24, 
+      },      
+      1200: {
+        slidesPerView: 4,
+        spaceBetween: 30,
+      }
+    }           
+  });
+
+  const productShowcaseSwiper = new Swiper('.product-showcase__list', {
+    loop: true,
+    scrollbar: {
+      el: '.product-showcase__list .swiper-scrollbar',        
+      draggable: true,
+    }, 
+    slidesPerView: 2.5,
+    spaceBetween: 10,    
+    breakpoints: {          
+      768: {
+        slidesPerView: 3.5,
+        spaceBetween: 20,
+      },
+      1280: {
+        slidesPerView: 4.5,
+        spaceBetween: 30,
+      }
+    }          
   });
   
   const initProductSwiper = (selector)=>{
